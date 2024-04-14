b08df05e381305227afa0bf5a55dc3a6d64935eb
YuHang-Diao <951900584@qq.com>
2024-04-14 21:25:06 +0800

    init

---
+++ src/main/java/com/api/bridge/ApiBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.api.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/bridge/ApiBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
