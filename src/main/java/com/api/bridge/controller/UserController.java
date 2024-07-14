1d0b3f74f37741d563ecdf50f0ba135d009d90f1
diaoyh <diaoyh@digiwin.com>
2024-07-14 16:03:11 +0800

    获取用户信息

--- src/main/java/com/api/bridge/controller/UserController.java
+++ src/main/java/com/api/bridge/controller/UserController.java
@@ -10,8 +10,10 @@ import com.api.bridge.dto.validGroup.Insert;
 import com.api.bridge.dto.validGroup.Select;
 import com.api.bridge.dto.validGroup.Update;
 import com.api.bridge.service.UserService;
+import com.api.bridge.utils.ReqThreadInfoUtil;
 import com.api.bridge.utils.SecretUtil;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,5 +46,11 @@ public class UserController {
     public ResultDto<String> editInfo(@RequestBody @Validated({Update.class}) UserReqDto userReqDto){
         User user = userService.editInfo(userReqDto);
         return ResultDto.createSuccess(Status.ok.getMessage());
+    }
+
+    @GetMapping("/getUserInfo")
+    public ResultDto getUserInfo(){
+        String userInfo = SecretUtil.decrypt(ReqThreadInfoUtil.getToken());
+        return ResultDto.createSuccess(JSONObject.parseObject(userInfo,UserResDto.class));
     }
 }
